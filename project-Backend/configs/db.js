127e598509c1e025b1956d1fadcd66e86784df6a
paper676 <paperr967@gmail.com>
2025-06-19 21:34:43 +0530

    DataBase Changet to MongoDB atlas(from local to cloud)

--- project-Backend/configs/db.js
+++ project-Backend/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectToDb=()=>{
-  mongoose.connect(process.env.DB_CONNECT).then(()=>{
+  mongoose.connect(`${process.env.DB_CONNECT}/QuickCommerce`).then(()=>{
     console.log("connected to database");
   }).catch(err=> console.log(err)
   )
