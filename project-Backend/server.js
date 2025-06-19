32628f2001e0a3ce7a8078073c44bcb67da7336b
paper676 <paperr967@gmail.com>
2025-06-19 20:33:43 +0530

    update in AllowedOrigins

--- project-Backend/server.js
+++ project-Backend/server.js
@@ -4,7 +4,7 @@ const cors=require('cors');
 
 const port=process.env.PORT || 3000;
 
-const AllowedOrigins=['http://localhost:5173','https://quick-commerce-project-frontend.vercel.app/']
+const AllowedOrigins=['http://localhost:5173','https://quick-commerce-project-frontend.vercel.app']
 
 const express=require('express');
 const app=express();
